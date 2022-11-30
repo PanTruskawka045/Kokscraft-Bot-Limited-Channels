f7301640b6c9b5a6d060929a17d3f10548ee623e
PanTruskawka045 - Mac <pan.truskawka045@gmail.com>
2022-11-30 22:03:05 +0100

    update

--- src/main/java/me/pan_truskawka045/LimitedChannels/listener/VoiceEventListener.java
+++ src/main/java/me/pan_truskawka045/LimitedChannels/listener/VoiceEventListener.java
@@ -21,25 +21,31 @@ public class VoiceEventListener {
     @EventHandler(event = Event.GUILD_MEMBER_VOICE_JOIN)
     private void onVoiceJoin(
             @ContextParam AudioChannel channel,
-            @ContextParam Member member,
-            @ContextParam Guild guild
-    ){
-        if(!(channel instanceof VoiceChannel)) return;
+            @ContextParam Member member
+    ) {
+        if (!(channel instanceof VoiceChannel)) return;
         VoiceChannel voiceChannel = (VoiceChannel) channel;
+        if(voiceChannel.getMembers().stream().anyMatch(m1 -> m1.getIdLong() != member.getIdLong())) return;
         long parentCategoryIdLong = voiceChannel.getParentCategoryIdLong();
         ChannelConfig channelConfig = limitedChannelsConfig.getChannels().getOrDefault(parentCategoryIdLong, null);
-        if(channelConfig == null) return;
-        List<Member> members = voiceChannel.getMembers();
-
+        if (channelConfig == null) return;
+        if(voiceChannel.getParentCategory() == null) return;
+        sortChannels(voiceChannel.getParentCategory());
     }
 
     @EventHandler(event = Event.GUILD_MEMBER_VOICE_LEAVE)
     private void onVoiceLeave(
             @ContextParam AudioChannel channel,
-            @ContextParam Member member,
-            @ContextParam Guild guild
-    ){
-
+            @ContextParam Member member
+    ) {
+        if (!(channel instanceof VoiceChannel)) return;
+        VoiceChannel voiceChannel = (VoiceChannel) channel;
+        if(voiceChannel.getMembers().isEmpty()) return;
+        long parentCategoryIdLong = voiceChannel.getParentCategoryIdLong();
+        ChannelConfig channelConfig = limitedChannelsConfig.getChannels().getOrDefault(parentCategoryIdLong, null);
+        if (channelConfig == null) return;
+        if(voiceChannel.getParentCategory() == null) return;
+        sortChannels(voiceChannel.getParentCategory());
     }
 
 
@@ -47,37 +53,43 @@ public class VoiceEventListener {
     private void onVoiceMove(
             @ContextParam AudioChannel left,
             @ContextParam AudioChannel joined,
-            @ContextParam Member member,
-            @ContextParam Guild guild
-            ){
-        onVoiceLeave(left, member, guild);
-        onVoiceJoin(joined, member, guild);
+            @ContextParam Member member
+    ) {
+        onVoiceLeave(left, member);
+        onVoiceJoin(joined, member);
     }
 
-    private void sortChannels(Category category){
+    private void sortChannels(Category category) {
         List<VoiceChannel> channels = category.getVoiceChannels();
         List<VoiceChannel> emptyChannels = category.getVoiceChannels();
         channels.sort(Comparator.comparing(VoiceChannel::getId));
         channels.removeIf(channel -> {
             boolean empty = channel.getMembers().isEmpty();
-            if(empty){
+            if (empty) {
                 emptyChannels.add(channel);
             }
             return empty;
         });
-        if(emptyChannels.size() > 1){
+
+        ChannelConfig channelConfig = limitedChannelsConfig.getChannels().get(category.getIdLong());
+        if (emptyChannels.isEmpty()) {
+            VoiceChannel channel = category.createVoiceChannel(channelConfig.getName().replace("{{index}}", "")).complete();
+            channels.add(channel);
+        } else if (emptyChannels.size() == 1) {
+            VoiceChannel channel = emptyChannels.get(0);
+            channels.add(channel);
+        } else {
             for (int i = 1; i < emptyChannels.size(); i++) {
                 emptyChannels.get(i).delete().queue();
             }
+            VoiceChannel channel = emptyChannels.get(0);
+            channels.add(channel);
         }
-        if(!emptyChannels.isEmpty()){
-            channels.add(emptyChannels.get(0));
-        }
-        ChannelConfig channelConfig = limitedChannelsConfig.getChannels().get(category.getIdLong());
+
         for (int i = 0; i < channels.size(); i++) {
             VoiceChannel channel = channels.get(i);
             channel.getManager().setPosition(i).setUserLimit(channelConfig.getLimit())
-                    .setName(channelConfig.getName().replace("{{index}}", String.valueOf(i+1))).queue();
+                    .setName(channelConfig.getName().replace("{{index}}", String.valueOf(i + 1))).queue();
         }
     }
 }
